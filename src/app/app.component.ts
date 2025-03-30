a7fd9e486ab30c3442c15864a46a15116787d75c
mayfor4 <diegoor.ti22@utsjr.edu.mx>
2025-03-30 10:55:57 -0600

    graficas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './pages/menu/menu.component';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,MenuComponent,FormsModule],
