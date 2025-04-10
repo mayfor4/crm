be50084e9e0a6c8da7e444c312af27930acf62a3
mayfor4 <diegoor.ti22@utsjr.edu.mx>
2025-04-10 17:12:45 -0600

    ya nose ve menu

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,26 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, computed, signal, effect } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-  authService : AuthService = inject(AuthService);
-  router : Router = inject(Router);
+  authService: AuthService = inject(AuthService);
+  router: Router = inject(Router);
 
-  salir(){
+  
+  user = signal<User | null>(null);
+
+  constructor() {
+    this.authService.user$.subscribe(this.user.set);
+  }
+
+  salir() {
     this.authService.logout().subscribe({
       next: () => {
         this.router.navigateByUrl('');
-      }, 
+      },
       error: (error) => {
         console.log('Error ', error);
       }
-    })
+    });
   }
-
 }
